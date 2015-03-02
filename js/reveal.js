24f8a4fe446a92351531b85fb1a371d51a586b52
santex <santex@grml.gchq.ml>
2015-03-02 01:47:55 +0100

    test

--- js/reveal.js
+++ js/reveal.js
@@ -439,8 +439,8 @@ function showMenu(x){
     
     v= v == 0 ? 1 : v;
     
-    if(h>=1 && v>=0) {
-    document.location.href="#"+h+"/"+v;
+    if(h && v) {
+   // document.location.href="#"+h+"/"+v;
     }
 
 
